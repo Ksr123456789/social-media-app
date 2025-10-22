b39d645c012b5f95458f29ce3c36eef8ecc1090d
Ksr123456789 <kuldeeprawat6277@gmail.com>
2025-10-22 12:00:58 +0530

    build basic express server and connect to database

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+
+server.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
+});
